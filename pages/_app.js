390b54266e30d865fec05194cc5769e2aba0e49f
HABIBKAGHAS <habibkaghas@gmail.com>
2022-08-29 16:53:47 +0300

    OLD TEMPLATE

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "../public/template/css/gradient.css";
+import "../public/template/css/layout.css";
+import "../public/template/css/demos/demo-2-colors.css";
+import "../public/template/css/template-colors/orange.css";
+import "../public/template/css/basic.css";
+import "../public/template/css/layout.css";
+import "../public/template/css/blogs.css";
+import "../public/template/css/ionicons.css";
+import "../public/template/css/magnific-popup.css";
+import "../public/template/css/animate.css";
+import "../public/template/css/gradient.css";
+// import "../public/template/css/new-skin/new-skin.css";
+import "../public/template/css/new-skin/classic-skin.css";
+import "../public/template/css/dark.css";
+// import "../public/template/css/owl.carousel.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
