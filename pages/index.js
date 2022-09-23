3730094221e9d8c2b64ea8841fb67a5bc701234d
HABIBKAGHAS <habibkaghas@gmail.com>
2022-09-24 01:37:22 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
         />
         <meta
           name="keywords"
-          content="Erbil, Best Developer in Erbil, Kurdistan Developer,  Web Developer, Full stack, Web3.0, DevOps, AWS, NodeJS, ReactJS."
+          content="Habib Kaghas, Erbil, Best Developer in Erbil, Kurdistan Developer,  Web Developer, Full stack, Web3.0, DevOps, AWS, NodeJS, ReactJS."
         />
         <meta
           name="description"
